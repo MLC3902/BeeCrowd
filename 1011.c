c1cf79ab3c5e56fb362279e0bdad989253230b3e
MariaLuiza <luizacardoso0303@gmail.com>
2026-05-15 08:07:29 -0300

    Create 1011.c

---
+++ 1011.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	double R, volume;
+	scanf("%lf",&R);
+	volume=(4.0/3.0)*3.14159*pow(R,3);
+	
+	printf("VOLUME = %.3f\n",volume);
+	return 0;
+}
